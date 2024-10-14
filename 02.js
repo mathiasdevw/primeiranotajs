9e752c2bda402b484d1dd64afebbdde6b515d78d
mathiasdevw <mathias_dev@hotmail.com>
2024-10-14 10:35:48 -0300

    Segunda questão

---
+++ 02.js
@@ -0,0 +1,10 @@
+function somanumero(numero){
+    soma = 0 
+
+    for(let i = 1; i <= numero; i++){
+            soma += i;         
+        }
+        return soma;
+    }
+
+    console.log(somanumero(10));
