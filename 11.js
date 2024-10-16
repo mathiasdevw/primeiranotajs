16aec46a691eaf18483f2530b6980b6da31edbe7
mathiasdevw <mathias_dev@hotmail.com>
2024-10-15 23:03:10 -0300

    Questão 11

--- 11.js
+++ 11.js
@@ -1,3 +1,10 @@
-function quadradosMaioresQueDez(numeros) {
-    return numeros.filter(x => x > 10).map(x => x ** 2);
+function quadradomaiorquedez(contador) {
+    return contador
+        .filter(numero => numero > 10)
+        .map(numero => numero * numero);
 }
+
+const numeros = [5, 12, 3, 18, 9, 37, 10, 17, 50, 2, 7, 9, 90];
+const resultado = quadradomaiorquedez(numeros);
+console.log(resultado); 
+
